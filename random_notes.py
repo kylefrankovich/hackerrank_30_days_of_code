b3b57c01dd30b372c68b36b7491e72f457e6140b
Kyle Frankovich <kylefrankovich@Kyles-MacBook-Pro.local>
2016-12-03 15:45:21 -0600

    a few more challenges

--- random_notes.py
+++ random_notes.py
@@ -84,3 +84,59 @@ print N ^ 0xffffffff
 
 
 print 1,2
+
+
+
+# class vs. instance:
+
+age = 34
+
+if age >= 13 and age < 18:
+    print 'You are a teenager.'
+
+
+
+
+class Person:
+    def __init__(self,initialAge):
+        # Add some more code to run some checks on initialAge
+        if initialAge < 0:
+            print 'Age is not valid, setting age to 0.'
+            age = 0
+        else:
+            age = initialAge
+    def amIOld(self):
+        # Do some computations in here and print out the correct statement to the console
+        if age < 13:
+            print 'You are young.'
+        elif age >= 13 and age < 18:
+            print 'You are a teenager.'
+        else:
+            print 'You are old.'
+    def yearPasses(self):
+        # Increment the age of the person in here
+        global age
+        age = age + 1
+
+
+t = int(raw_input())
+for i in range(0, t):
+    age = int(raw_input())
+    p = Person(age)
+    p.amIOld()
+    for j in range(0, 3):
+        p.yearPasses()
+    p.amIOld()
+    print("")
+
+
+
+# loops:
+
+for i in range(1,11):
+    answer = 2 * i
+    print '2 X %d = %d' % (i, answer)
+
+
+
+
