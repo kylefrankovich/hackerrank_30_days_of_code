4da3177c0e27e8301342e03cdb5f9aaf14aab3cd
Kyle Frankovich <kylefrankovich@kyles-mbp.attlocal.net>
2017-03-13 21:51:03 -0500

    getting back to hackerrank

--- random_notes.py
+++ random_notes.py
@@ -580,3 +580,21 @@ printf("%d",maxsum);
 
 # day 12 (Inheritance):
 
+
+class Student(Person):
+
+    # Class Constructor
+    #
+    #   Parameters:
+    #   firstName - A string denoting the Person's first name.
+    #   lastName - A string denoting the Person's last name.
+    #   id - An integer denoting the Person's ID number.
+    #   scores - An array of integers denoting the Person's test scores.
+    #
+    # Write your constructor here
+
+
+    #   Function Name: calculate
+    #   Return: A character denoting the grade.
+    #
+    # Write your function here
